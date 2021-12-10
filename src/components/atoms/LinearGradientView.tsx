d7f9b1da33f8660899cd76960032d239bf478766
yuchida-tamu <yutaside1992@gmail.com>
2021-12-11 04:10:46 +0900

    feat: add LinearGradientView

---
+++ src/components/atoms/LinearGradientView.tsx
@@ -0,0 +1,32 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { ReactElement, ReactEventHandler } from 'react';
+import { StyleProp, View, ViewStyle } from 'react-native';
+import { boxShadow } from '../../styles/styles';
+
+type LinneaGradientViewProps = {
+  color1: string;
+  color2: string;
+  style?: StyleProp<ViewStyle>;
+  isBoxShadow?: boolean;
+  children?: ReactElement;
+  start?: { x: number; y: number };
+  end?: { x: number; y: number };
+};
+
+export const LinearGradientView: React.FC<LinneaGradientViewProps> = ({
+  color1,
+  color2,
+  isBoxShadow,
+  style,
+  children,
+  start,
+  end,
+}) => {
+  return (
+    <View style={isBoxShadow && boxShadow}>
+      <LinearGradient style={style} colors={[color1, color2]} start={start} end={end}>
+        {children}
+      </LinearGradient>
+    </View>
+  );
+};
