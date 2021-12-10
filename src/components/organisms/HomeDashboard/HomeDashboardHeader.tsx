4830342937390294b01e41c181a6bb875383d68a
yuchida-tamu <yutaside1992@gmail.com>
2021-12-11 04:19:34 +0900

    refactor: use LinearGradientView

--- src/components/organisms/HomeDashboard/HomeDashboardHeader.tsx
+++ src/components/organisms/HomeDashboard/HomeDashboardHeader.tsx
@@ -2,20 +2,21 @@ import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { View, Text } from 'react-native';
 import { COLOR } from '../../../constants/colors';
+import { LinearGradientView } from '../../atoms/LinearGradientView';
 import { styles } from './styles';
 
 export const HomeDashboardHeader = () => {
   const today = new Date();
 
   return (
-    <View style={styles.headerContainer}>
-      <LinearGradient
-        style={styles.headerBackground}
-        colors={[COLOR.PRIMARY, COLOR.bg.gradient.TEAL]}>
-        <Text style={styles.headerText}>
-          {`${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`}
-        </Text>
-      </LinearGradient>
-    </View>
+    <LinearGradientView
+      style={styles.headerContainer}
+      color1={COLOR.PRIMARY}
+      color2={COLOR.bg.gradient.TEAL}
+      isBoxShadow={true}>
+      <Text style={styles.headerText}>
+        {`${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`}
+      </Text>
+    </LinearGradientView>
   );
 };
