cbd9661acb1c88bcdb0b5403d15a8c0a04892ca3
yuchida-tamu <yutaside1992@gmail.com>
2021-12-12 12:49:23 +0900

    feat: format

--- src/store/reducers/appReducer.ts
+++ src/store/reducers/appReducer.ts
@@ -4,9 +4,9 @@ import { AppModel, AppModelType } from '../models/app/app';
 
 export function appReducer(data: AppModelType = AppModel.create(), action: AppActions) {
   switch (action.type) {
-    case ActionTypes.R_UPDATE_PROCESSING_STATUS:
-      return AppModel.updateIsProcessing(data, action.payload.isProcessing);
-    default:
-      return data;
+  case ActionTypes.R_UPDATE_PROCESSING_STATUS:
+    return AppModel.updateIsProcessing(data, action.payload.isProcessing);
+  default:
+    return data;
   }
 }
