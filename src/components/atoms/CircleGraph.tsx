3a3049b8f8f709eba148cbd64d515543bc47e301
yuchida-tamu <yutaside1992@gmail.com>
2022-01-10 11:52:13 +0900

    chore: add circle graph but remove it from dashboard

---
+++ src/components/atoms/CircleGraph.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleProp, View, ViewStyle } from 'react-native';
+import Svg, { Circle, Path } from 'react-native-svg';
+import { COLOR } from '../../constants/colors';
+type Props = {
+  height: number;
+  strokeWidth: number;
+  style?: StyleProp<ViewStyle>;
+  color: string;
+  radius: number;
+  startAngle: number;
+  endAngle: number;
+};
+
+export const CircleGraph: React.FC<Props> = ({
+  height,
+  color,
+  strokeWidth,
+  style,
+  radius,
+  startAngle,
+  endAngle,
+}) => {
+  const cx = height / 2;
+  const cy = height / 2;
+
+  const d = describeArc(cx, cy, radius, startAngle, endAngle);
+
+  return (
+    <View style={style}>
+      <Svg width={height} height={height}>
+        <Path d={d} stroke={color} strokeWidth={strokeWidth} />
+        <Circle
+          cx={height / 2}
+          cy={height / 2}
+          r={height / 2 - strokeWidth}
+          fill={COLOR.GRAY}
+          strokeWidth={strokeWidth}
+        />
+      </Svg>
+    </View>
+  );
+};
+
+function polarToCartesian(
+  centerX: number,
+  centerY: number,
+  radius: number,
+  angleInDegrees: number,
+) {
+  const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians),
+  };
+}
+
+function describeArc(x: number, y: number, radius: number, startAngle: number, endAngle: number) {
+  const start = polarToCartesian(x, y, radius, endAngle);
+  const end = polarToCartesian(x, y, radius, startAngle);
+
+  const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+
+  const d = ['M', start.x, start.y, 'A', radius, radius, 0, arcSweep, 0, end.x, end.y].join(' ');
+
+  console.log(d);
+
+  return d;
+}
--- src/components/organisms/HomeDashboard/HomeDashboardProgramInfoItem.tsx
+++ src/components/organisms/HomeDashboard/HomeDashboardProgramInfoItem.tsx
@@ -3,12 +3,9 @@ import { Text, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import { COLOR } from '../../../constants/colors';
 import { numberOfProgramsSelector } from '../../../store/selectors/user/userSelector';
-import { CircleGraphSkelton } from '../../atoms/CircleGraphSkelton';
 import { LinearGradientView } from '../../atoms/LinearGradientView';
 import { HomeDashboardItemWrapper } from '../../molecules/HomeDashboard/HomeDashboardWrapper';
-import { ITEM_HEIGHT, styles } from './styles';
-
-const strokeWidth = 20;
+import { styles } from './styles';
 
 type Props = {
   navigate: () => void;
@@ -36,11 +33,6 @@ export const HomeDashboardProgramInfoItem: React.FC<Props> = ({ navigate }) => {
             </>
           </LinearGradientView>
         </TouchableOpacity>
-        <CircleGraphSkelton
-          strokeWidth={strokeWidth}
-          height={ITEM_HEIGHT}
-          style={styles.dashboardItemGraphConatainer}
-        />
       </>
     </HomeDashboardItemWrapper>
   );
