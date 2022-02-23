60cec5aa7f4e60b4502509692cf8855db6096883
yuchida-tamu <yutaside1992@gmail.com>
2022-02-23 19:58:00 +0900

    chore: change the style of program num card

--- src/components/organisms/HomeDashboard/styles.ts
+++ src/components/organisms/HomeDashboard/styles.ts
@@ -43,7 +43,7 @@ export const styles = StyleSheet.create({
     paddingVertical: SPACING.XSMALL,
   },
   dashboardItemText: {
-    color: COLOR.text.BLACK,
+    color: COLOR.text.WHITE,
     textAlign: 'center',
   },
   dashboardItemHeadingText: {
